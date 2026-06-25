51080379ab73b1a9f8de25644267f9923980f9b6
condegia2005-lgtm <condegia2005@gmail.com>
2026-06-25 23:12:05 +0700

    Add files via upload

---
+++ supabase/migrations/20260616185405_0a673935-3cd3-4715-9118-4334b9b49c46.sql
@@ -0,0 +1,12 @@
+
+UPDATE public.products SET image_url = '/__l5e/assets-v1/eb6173b6-2dbc-4ace-9d47-1fc3ca8863bc/product1.png' WHERE name = 'Phin Sữa Đá';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/9bf3bcc2-61a0-43d7-a40f-1294d4db2e83/product-2.png' WHERE name = 'Phin Đen Đá';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/cebc2554-639b-44b0-9b1b-0de7733c03da/product-3.png' WHERE name = 'Bạc Xỉu Đá';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/e51d4f78-36fb-474d-8680-dc527891aff6/product-4.png' WHERE name = 'Espresso';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/6d76d768-3342-4961-9092-1c4b3f88554f/product-6.png' WHERE name = 'Americano';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/2420bd10-036c-47b9-9838-424f55b00c1b/product-9.png' WHERE name = 'Caramel Macchiato';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/1a1e32f2-b629-4c09-9bea-dacb48367965/product-7.png' WHERE name = 'Freeze Trà Xanh';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/7a9b1f94-c7df-4b00-819d-05b66a867789/product-8.png' WHERE name = 'Freeze Cookies & Cream';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/35142363-62a6-4dc9-ac36-d7520e78c4b6/product-5.png' WHERE name = 'Trà Sen Vàng';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/35142363-62a6-4dc9-ac36-d7520e78c4b6/product-5.png' WHERE name = 'Trà Thạch Đào';
+UPDATE public.products SET image_url = '/__l5e/assets-v1/1a1e32f2-b629-4c09-9bea-dacb48367965/product-7.png' WHERE name = 'Trà Xanh Đậu Đỏ';
